4a4aef8281baab8f1d882bb728468f9ae4a13499
nirmala2810 <nirmala.elsha@gmail.com>
2023-02-08 21:49:53 +0000

    Final code

--- src/test/java/org/test/TestMain.java
+++ src/test/java/org/test/TestMain.java
@@ -1,5 +1,10 @@
 package org.test;
 
 public class TestMain {
+	public static void main(String[]args) {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+	}
 
 }
